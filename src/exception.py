a9195bb751197d9c50b56369bf2628b744312b80
sweta nishad <Swetanishad018@gmail.com>
2024-04-28 16:16:59 +0530

    Project structure

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+
+def error_message_details(error, error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_massage = "Error occured in python Script name [{0}] line number [{1}] error massage [{2}]".format(
+    file_name,exc_tb.tb_lineno, str(error))
+    
+    return error_massage
+
+
+    class CustomExeption(Exception):
+        def __init__(self, error_massage , error_details:sys):
+            super().__init__(error_massage)
+            self.error_massage = error_massage_detail(error_massage,error_details= error_details)
+
+       
+
+      
+

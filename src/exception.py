2fa671066ea82a1de8caa5e13d714344e59cc775
sweta nishad <Swetanishad018@gmail.com>
2024-04-30 22:18:13 +0530

    EDA and problem statements

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,27 @@
 import sys
+from logger import logging 
 
-def error_message_details(error, error_details:sys):
-    _,_,exc_tb = error_details.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_massage = "Error occured in python Script name [{0}] line number [{1}] error massage [{2}]".format(
-    file_name,exc_tb.tb_lineno, str(error))
+
+
+def error_massage_details(error,error_details:sys):
+        _,_,exc_tb = error_details.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        error_massage = " error occur in python script name  [{0}] line number [{1} error massage [{2}]]".format(
+        file_name,exc_tb.tb_lineno, str(error))
+
+        return error_massage
+        
+class CustomException(Exception):
+        def __init__(self, error_massage, error_details:sys):
+                super.__init__(error_massage)
+                self.error_message= error_massage_details(error_massage , error_details=error_details)
+
+        def __str__(self):
+               return self.error_message
+if __name__== "__main__":
     
-    return error_massage
-
-
-    class CustomExeption(Exception):
-        def __init__(self, error_massage , error_details:sys):
-            super().__init__(error_massage)
-            self.error_massage = error_massage_detail(error_massage,error_details= error_details)
-
-       
-
-      
-
+    try:
+        a =1/10
+    except Exception as e:
+        logging.info('divied by zero ')
+        raise CustomException(e,sys)
